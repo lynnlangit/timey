311e1668248c99836bea703ac6e269efc077cb23
Lynn Langit <lynnlangit@live.com>
2016-06-30 11:48:58 -0700

    added actual path

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,9 @@ grunt.initConfig({
     angular_architecture_graph: {
             diagram: {
                 files: {
-                    // "PATH/TO/OUTPUT/FILES": ["PATH/TO/YOUR/FILES/*.js"]
+                    "PATH/TO/OUTPUT/FILES": [
+                        "/Users/lynnlangit/Documents/git/timey/*.js"
+                        ],
                     "architecture": [
                         "<%= projectConfig.app %>/<%= projectConfig.project %>/**/*.js"
                     ]
